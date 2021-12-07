d9762dfde7ac492bc05fa6b19887a3f84a56e527
alvaro004 <alvarollano04@gmail.com>
2021-12-07 11:39:06 -0300

    First demo for catlog section

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,34 @@
+// let swiper = new Swiper('.mySwiper', {
+// 	cssMode: true,
+// 	loop: true,
+// 	autoplay: {
+// 		delay: 3000,
+// 		disableOnInteraction: false,
+// 	},
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		clickable: true,
+// 	},
+// 	mousewheel: true,
+// 	keyboard: true,
+// });
+
 let swiper = new Swiper('.mySwiper', {
-	cssMode: true,
+	slidesPerView: 3,
+	spaceBetween: 30,
+	slidesPerGroup: 3,
 	loop: true,
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
 	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-	mousewheel: true,
-	keyboard: true,
 });
