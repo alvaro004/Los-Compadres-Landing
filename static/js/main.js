55e0b226ff19256d2f8a796089438f3605336131
alvaro004 <alvarollano04@gmail.com>
2021-12-07 17:03:53 -0300

    products banner added

--- static/js/main.js
+++ static/js/main.js
@@ -27,10 +27,10 @@ let swiper = new Swiper('.mySwiper', {
 		el: '.swiper-pagination',
 		clickable: true,
 	},
-	// autoplay: {
-	// 	delay: 5000,
-	// 	disableOnInteraction: false,
-	// },
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
