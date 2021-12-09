90a598b4f358437a162ff78c66dff8f67115dc08
alvaro004 <alvarollano04@gmail.com>
2021-12-08 21:05:21 -0300

    adding img for the banner

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,3 @@
-// let swiper = new Swiper('.mySwiper', {
-// 	cssMode: true,
-// 	loop: true,
-// 	autoplay: {
-// 		delay: 3000,
-// 		disableOnInteraction: false,
-// 	},
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-// 	pagination: {
-// 		el: '.swiper-pagination',
-// 		clickable: true,
-// 	},
-// 	mousewheel: true,
-// 	keyboard: true,
-// });
-
 let swiper = new Swiper('.mySwiper', {
 	slidesPerView: 3,
 	spaceBetween: 30,
